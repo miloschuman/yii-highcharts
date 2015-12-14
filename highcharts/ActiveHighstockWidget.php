95bd55976867e5a09d3a000094cfe905e994b8e0
milo <miloschuman@gmail.com>
2015-12-14 13:08:41 -0500

    Upgraded Highcharts core library to v4.1.10

--- highcharts/ActiveHighstockWidget.php
+++ highcharts/ActiveHighstockWidget.php
@@ -6,7 +6,7 @@
  * @author David Baker <github@acorncomputersolutions.com>
  * @link https://github.com/miloschuman/yii-highcharts/
  * @license http://www.opensource.org/licenses/mit-license.php MIT License
- * @version 4.1.9
+ * @version 4.1.10
  */
 
 require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'HighstockWidget.php');
--- highcharts/HighmapsWidget.php
+++ highcharts/HighmapsWidget.php
@@ -6,7 +6,7 @@
  * @author Milo Schuman <miloschuman@gmail.com>
  * @link https://github.com/miloschuman/yii-highcharts/
  * @license http://www.opensource.org/licenses/mit-license.php MIT License
- * @version 4.1.9
+ * @version 4.1.10
  */
 require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'HighchartsWidget.php');
 
